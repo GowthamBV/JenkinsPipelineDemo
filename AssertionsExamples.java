f5d8963194ea7290b4e042a0624a2431e513cb57
Gowtham <ggowthu1998@gmail.com>
2022-09-15 11:42:58 +0530

    JunitTest Cases

---
+++ AssertionsExamples.java
@@ -0,0 +1,42 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AssertionsExamples {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test1() {
+        Assert.assertFalse(false);
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertEquals("Test", "Test");
+    }
+
+    @Test
+    public void test3() {
+        Assert.assertEquals("Test", "Test");
+    }
+
+    @Test
+    public void test4() {
+        Assert.assertTrue(true, "matching the expected condition");
+    }
+
+    @Test
+    public void test5() {
+        Assert.assertFalse(false, " This is expected condition");
+    }
+
+    @Test
+    public void test6() {
+        Assert.assertNotEquals("test", "test1");
+    }
+}
+
+
---
+++ FirstTestCase.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.Test;
+
+public class FirstTestCase {
+
+    @Test(priority = 1)
+    void setup()
+    {
+        System.out.println("This is setup test");
+    }
+
+    @Test(priority = 2)
+    void login()
+    {
+        System.out.println("This is login test");
+    }
+
+    @Test(priority = 3)
+    void down()
+    {
+        System.out.println("This is Down test");
+    }
+
+}
